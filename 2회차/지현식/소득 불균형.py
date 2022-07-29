1f7b67d4bf7677ccf4ac276acbfb45c1bf4fb952
HYUNSIK-JI <https://github.com/HYUNSIK-JI>
2022-07-29 15:18:32 +0900

    오타 수정

--- 2회차/지현식/소득 불균형.py
+++ 2회차/지현식/소득 불균형.py
@@ -5,6 +5,6 @@ for test_case in range(1, int(input()) + 1):
     answer = 0
     aver = sum(income) // n
     for i in income:
-        if aver <= i:
+        if i <= aver :
             answer += 1
     print(f"#{test_case} {answer}")
